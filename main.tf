95a8860fe896dba41ba364fc748ac33fcef131ee
muralialltest <108783484+muralialltest@users.noreply.github.com>
2022-12-19 12:20:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+output "random_password" {
+ value= random_password.password
+}
